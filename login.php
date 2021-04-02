ed97d33f310671b9d937b9406cb86658bc9589cb
anthonyaboukhalil <72323137+anthonyaboukhalil@users.noreply.github.com>
2021-04-02 10:53:56 -0700

    Added Log In page

---
+++ login.php
@@ -0,0 +1,34 @@
+
+
+<head>
+<title>Login</title>
+
+<h1>Our Blog Site</h1>
+
+<link rel= "stylesheet" href="styles.css">
+
+</head>
+<body>
+<div class = "form-master">
+   <form action="">
+
+
+    <div class= "email-form">
+    <input  type="text" name="e-mail" required>
+    <label for="e-mail" class ="e-label"><span class = "email-content">E-mail</span></label>
+    </div>
+    <div class ="spacing"></div>
+    <div class= "password-form">
+    <input type="password" name="pass" required>
+    <label for="pass"><span class = "password-content">Password</span></label>
+    </div>
+   <button type = "submit" class = "submit"> <span class="button-content"> Log In </span>
+    </button>
+   
+    <div class="links">
+   <a href="#" class ="reset"> <p>Forgot password ?</p></a>
+   <a href="#" class ="register"> <p>Register</p></a>
+   </div> 
+</form>
+</div>
+</body>
