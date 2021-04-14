96d93a9b965c36e6a03f759f9fa8a7814ad7a8eb
Joels17 <joel.semeniuk@gmail.com>
2021-04-13 18:23:29 -0700

    Database connection

--- scripts/database_connect.php
+++ scripts/database_connect.php
@@ -1,10 +1,19 @@
 
 <?php
-// Included where db connection is needed
-$host = "localhost";
-$database = "project360";
-$user = "webuser";
-$password = "P@ssw0rd";
+// Local db connection
+// $host = "localhost";
+// $database = "project360";
+// $user = "webuser";
+// $password = "P@ssw0rd";
+
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$host = $cleardb_url["host"];
+$user = $cleardb_url["user"];
+$password = $cleardb_url["pass"];
+$database = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
 
 $connection = mysqli_connect($host, $user, $password, $database);
 
