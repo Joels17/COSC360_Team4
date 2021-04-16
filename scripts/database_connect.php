13f2473bb04da9573970f4f7e40d0c78b83f7fe8
Kyle <59974201+Dolade@users.noreply.github.com>
2021-04-16 15:47:54 -0700

    Very close to finishing

--- scripts/database_connect.php
+++ scripts/database_connect.php
@@ -1,17 +1,17 @@
 
 <?php
 // Included where db connection is needed
-$host = "localhost";
-$database = "project360";
-$user = "webuser";
-$password = "P@ssw0rd";
+//$host = "localhost";
+//$database = "project360";
+//$user = "webuser";
+//$password = "P@ssw0rd";
 
 //Get Heroku ClearDB connection information
 
-// $host = "us-cdbr-east-03.cleardb.com";
-// $database = "heroku_3a9bf1367309f32";
-// $user = "bb9e705b04bc79";
-// $password = "1e1b9a6f";
+ $host = "us-cdbr-east-03.cleardb.com";
+ $database = "heroku_3a9bf1367309f32";
+ $user = "bb9e705b04bc79";
+ $password = "1e1b9a6f";
 
 
 //change these values below to local if needed
