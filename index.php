03dd85b5deb84c948838398ccbeeafa8f4cc8d25
Kyle <59974201+Dolade@users.noreply.github.com>
2021-04-16 12:55:18 -0700

    Almost done

--- index.php
+++ index.php
@@ -63,10 +63,66 @@
 							mysqli_close($connection);
 					?>
 					
-					<!-- DIV CLASS ROW IS ADDED INSIDE HERE -->
+					<!-- END OF DIV CLASS ROW ADDED HERE -->
 				</div>
 		</div>
 		<div class="column-right" id="column2">
+			<?php 
+                if(isset($_SESSION['user'])){
+            ?>
+				<h3>Your Posts</h3>
+				<div id="right-wrapper">
+					<!-- DIV CLASS ROW IS ADDED INSIDE HERE -->
+					<?php
+						include "scripts/database_connect.php";
+						
+						$sqlMyBlogs = "SELECT title, views FROM blogs B WHERE uploadUser = ?;";
+						$stmtCheck = mysqli_stmt_init($connection);
+						$username = $_SESSION['user'];
+						if(!mysqli_stmt_prepare($stmtCheck, $sqlMyBlogs)){
+							echo "SQL statement failed";
+						}else{
+							mysqli_stmt_bind_param($stmtCheck, "s", $username);
+							mysqli_stmt_execute($stmtCheck);
+							$resultsCheck = mysqli_stmt_get_result($stmtCheck);
+							while ($myRow = mysqli_fetch_assoc($resultsCheck))
+							{
+								echo "<div id=\"row\">";
+									echo "<div id=\"post\">";
+										echo "<div id=\"preset-row\">";
+											echo "<div id=\"row\">";
+												echo "<div id=\"blog-image\">";
+													echo "<img src=\"https://media.defense.gov/2020/Feb/19/2002251686/700/465/0/200219-A-QY194-002.JPG\" width=90 height=90/>";
+												echo "</div>";
+												echo "<div id=\"spacer\">";
+												echo "</div>";
+												echo "<div id=\"blog-info\">";
+													echo "<div id=\"row\">";
+														echo "<div id=\"info-title\" class=\"info-inside\">";
+															echo "Title: " . $myRow['title'];
+														echo "</div>";
+														echo "<div id=\"info-author\" class=\"info-inside\">";
+															echo "Author: Me";
+														echo "</div>";
+														echo "<div id=\"info-views\" class=\"info-inside\">";
+															echo "Views: " . $myRow['views'];
+														echo "</div>";
+													echo "</div>";
+												echo "</div>";
+											echo "</div>";
+										echo "</div>";
+									echo "</div>";
+								echo "</div>";
+							}
+							
+						}
+							mysqli_close($connection);
+					?>
+					
+					<!-- END OF DIV CLASS ROW ADDED HERE -->
+				</div>
+				</div>
+			<?php }else{ ?>
 			<div id="box-container">
 				<p class="login-text">
 					Interested in making your own Blog post?
@@ -85,6 +141,7 @@
 					</div>
 				</div>
 			</div>
+			<?php } ?>
 		</div>
     </div>
 
